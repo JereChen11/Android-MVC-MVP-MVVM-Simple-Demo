c1004c8352da4f6142651719020f2f5fac3920be
jere <jere@motherapp.com>
2019-08-25 13:40:54 +0800

    update MVC design pattern

---
+++ app/src/main/java/com/example/mvcmvpmvvm/mvc/veiwandcontroller/MvcLoginActivity.java
@@ -0,0 +1,54 @@
+package com.example.mvcmvpmvvm.mvc.veiwandcontroller;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.mvcmvpmvvm.R;
+import com.example.mvcmvpmvvm.mvp.model.User;
+
+/**
+ * @author jere
+ */
+public class MvcLoginActivity extends AppCompatActivity {
+    private EditText userNameEt;
+    private EditText passwordEt;
+    private User user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mvc_login);
+
+        user = new User();
+        userNameEt = findViewById(R.id.user_name_et);
+        passwordEt = findViewById(R.id.password_et);
+        Button loginBtn = findViewById(R.id.login_btn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login(userNameEt.getText().toString(), passwordEt.getText().toString());
+            }
+        });
+    }
+
+    private void login(String userName, String password) {
+        if (userName.equals("jere") && password.equals("123")) {
+            user.setUserName(userName);
+            user.setPassword(password);
+            Toast.makeText(MvcLoginActivity.this,
+                    userName + " Login Successful",
+                    Toast.LENGTH_SHORT)
+                    .show();
+        } else {
+            Toast.makeText(MvcLoginActivity.this,
+                    "Login Failed",
+                    Toast.LENGTH_SHORT)
+                    .show();
+        }
+    }
+}
